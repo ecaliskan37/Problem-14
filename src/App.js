578c60ca42e41262e5eb917997c228f6510762db
ecaliskan37 <ebru-1-mat@hotmail.com>
2025-07-25 21:36:03 +0300

    ddd

--- src/App.js
+++ src/App.js
@@ -1,121 +1,219 @@
-import { TrashIcon } from '@heroicons/react/20/solid'
+import { TrashIcon, PlusIcon, MinusIcon } from '@heroicons/react/20/solid'
 import Image from 'next/image'
+import { useState } from 'react'
 
 const cartProducts = [
   {
     id: 1,
     title: 'Basic Tee',
     href: '#',
-    price: '$32.00',
+    cargo: '$10.00',
     color: 'Black',
     size: 'Large',
     imageSrc: '/product-01.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
+    amount: 1,
+    birim: 32,
+    delete: false,
   },
   {
     id: 2,
     title: 'Basic Tee',
     href: '#',
-    price: '$32.00',
+    cargo: '$5.00',
     color: 'Sienna',
     size: 'Large',
     imageSrc: '/product-02.jpg',
     imageAlt: "Front of men's Basic Tee in sienna.",
+    amount: 2,
+    birim: 16,
+    delete: false,
   },
 ]
 
-// Sipariş özeti içeren alışveriş sepeti görünümü sağlıyoruz.
-// Görevler:
-// 1. "Çöp kutusu" ikonuna tıklayarak listedeki ürünleri kaldırmayı mümkün hale getirin.
-//    - Ürün silindiğinde, ara toplam, kargo ve toplam fiyat değerlerinin doğru şekilde güncellenmesini sağlayın.
-// 2. Ara toplam, kargo ve toplam fiyat değerlerini dinamik hale getirin. (Kargo ücreti sabit olabilir ancak ara toplam ürünlerin fiyatına göre hesaplanmalıdır.)
-// 3. "Siparişi onaylayın" butonuna tıklandığında, console.log() ile mevcut sipariş özeti bilgilerini (ürünler, ara toplam, toplam) görüntüleyin.
-// Bonus:
-// - Ürün kaldırıldığında kullanıcıya doğrulama penceresi gösterin ("Bu ürünü kaldırmak istediğinizden emin misiniz?").
-// - Sepette ürün kalmadığında ara toplam, toplam ve kargo bilgilerini gizleyerek "Sepetiniz boş" mesajını görüntüleyin.
-// - Responsive tasarımı optimize edin: Mobil cihazlarda ürün bilgileri dikey bir düzende gösterilsin.
-// - Her ürün için dinamik bir fiyat hesaplama ekleyin (miktar seçimi ve birim fiyat çarpımı).
-// Tailwind ile ilgili bonus istekler:
-// 1. Ürün kaldırma butonuna tıklandığında, buton geçici olarak "Kaldırılıyor..." durumunu gösterecek şekilde stil ekleyin.
-// 2. Toplam fiyat değerine dikkat çeken vurgu (daha büyük yazı boyutu veya farklı bir arka plan rengi) ekleyin.
-// 3. Her ürünün fiyatını hover sırasında vurgulamak için görsel efektler ekleyin ( fiyat metninin rengini değiştirin).
-
 export default function OrderSummary() {
-  return (
-    <div className='max-w-sm py-8 mx-auto'>
-      <h2 className='text-lg font-medium text-gray-900'>Sipariş özeti</h2>
+  const [item, setItem] = useState(cartProducts)
 
-      <div className='mt-4 rounded-lg border border-gray-200 bg-white shadow-sm'>
-        <h3 className='sr-only'>Alışveriş sepetinizdeki ürünler</h3>
-        <ul role='list' className='divide-y divide-gray-200'>
-          {cartProducts.map((product) => (
-            <li key={product.id} className='flex px-4 py-6 sm:px-6'>
-              <div className='flex-shrink-0'>
+  const deleteItem = (id) => {
+    const answ = confirm('Bu ürünü kaldırmak istediğinizden emin misiniz?')
+    if (answ) {
+      setTimeout(() => {
+        const list = item.filter((item) => item.id != id)
+        setItem(list)
+      }, 3000)
+    }
+  }
+
+  const addCount = (id) => {
+    const list = item.map((data) => {
+      if (data.id == id) {
+        return { ...data, amount: data.amount + 1 }
+      }
+      return data
+    })
+    console.log(list)
+    setItem(list)
+  }
+
+  const minusCount = (id) => {
+    const list = item.map((data) => {
+      if (data.id == id) {
+        return { ...data, amount: data.amount - 1 }
+      }
+      return data
+    })
+    setItem(list)
+  }
+
+  const handleClick = () => {
+    console.log(item)
+  }
+
+  return (
+    <div className="max-w-sm py-8 mx-auto md:flex flex-col">
+      <h2 className="text-lg font-medium text-gray-900">Sipariş özeti</h2>
+
+      <div className="mt-4 rounded-lg border border-gray-200 bg-white shadow-sm">
+        <h3 className="sr-only">Alışveriş sepetinizdeki ürünler</h3>
+        <ul role="list" className="divide-y divide-gray-200">
+          {item.map((product) => (
+            <li key={product.id} className="flex px-4 py-6 sm:px-6">
+              <div className="flex-shrink-0">
                 <Image
                   src={product.imageSrc}
                   alt={product.imageAlt}
-                  className='w-20 rounded-md'
+                  className="w-20 rounded-md"
                   width={500}
                   height={500}
                 />
               </div>
 
-              <div className='ml-6 flex flex-1 flex-col'>
-                <div className='flex'>
-                  <div className='min-w-0 flex-1'>
-                    <h4 className='text-sm'>
+              <div className="ml-6 flex flex-1 flex-col">
+                <div className="flex">
+                  <div className="min-w-0 flex-1">
+                    <h4 className="text-sm">
                       <a
                         href={product.href}
-                        className='font-medium text-gray-700 hover:text-gray-800'
+                        className="font-medium text-gray-700 hover:text-gray-800"
                       >
                         {product.title}
                       </a>
                     </h4>
-                    <p className='mt-1 text-sm text-gray-500'>
+                    <p className="mt-1 text-sm text-gray-500">
                       {product.color}
                     </p>
-                    <p className='mt-1 text-sm text-gray-500'>{product.size}</p>
+                    <p className="mt-1 text-sm text-gray-500">{product.size}</p>
                   </div>
-
-                  <div className='ml-4 flow-root flex-shrink-0'>
+                  <div className="ml-4 flow-root flex-shrink-0">
                     <button
-                      type='button'
-                      className='-m-2.5 flex items-center justify-center bg-white p-2.5 text-gray-400 hover:text-gray-500'
+                      type="button"
+                      className="-m-2.5 flex items-center justify-center bg-white p-2.5 text-gray-400 hover:text-gray-500"
+                      onClick={() => {
+                        const list = item.map((item) => {
+                          if (item.id == product.id) {
+                            return { ...item, delete: true }
+                          }
+                          return item
+                        })
+                        setItem(list)
+                        deleteItem(product.id)
+                      }}
                     >
-                      <span className='sr-only'>Kaldır</span>
-                      <TrashIcon className='h-5 w-5' aria-hidden='true' />
+                      <span className="sr-only">Kaldır</span>
+                      <TrashIcon className="h-5 w-5" aria-hidden="true" />
+                    </button>
+                    <button
+                      type="button"
+                      className="-m-2.5 flex items-center justify-center bg-white p-2.5 text-gray-400 hover:text-gray-500"
+                      onClick={() => addCount(product.id)}
+                      disabled={product.amount == 10}
+                    >
+                      <span className="sr-only">Kaldır</span>
+                      <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                    </button>
+                    <div className="-m-2.5 flex items-center justify-center">
+                      {product.amount}
+                    </div>
+                    <button
+                      type="button"
+                      className="-m-2.5 flex items-center justify-center bg-white p-2.5 text-gray-400 hover:text-gray-500"
+                      onClick={() => minusCount(product.id)}
+                      disabled={product.amount == 0}
+                    >
+                      <span className="sr-only">Kaldır</span>
+                      <MinusIcon className="h-5 w-5" aria-hidden="true" />
                     </button>
                   </div>
                 </div>
 
-                <div className='flex flex-1 items-end justify-between pt-2'>
-                  <p className='mt-1 text-sm font-medium text-gray-900'>
-                    {product.price}
+                <div className="flex flex-1 items-end justify-between pt-2">
+                  <p className="mt-1 text-sm font-medium text-gray-900                 hover:text-amber-200">
+                    ${product.birim * product.amount}
                   </p>
                 </div>
+                {product.delete && (
+                  <div>
+                    <span class="font-medium">.....Ürün Kaldırılıyor...</span>
+                  </div>
+                )}
               </div>
             </li>
           ))}
         </ul>
-        <dl className='space-y-6 border-t border-gray-200 px-4 py-6 sm:px-6'>
-          <div className='flex items-center justify-between'>
-            <dt className='text-sm'>Ara Toplam</dt>
-            <dd className='text-sm font-medium text-gray-900'>$64.00</dd>
-          </div>
-          <div className='flex items-center justify-between'>
-            <dt className='text-sm'>Kargo</dt>
-            <dd className='text-sm font-medium text-gray-900'>$5.00</dd>
-          </div>
-          <div className='flex items-center justify-between border-t border-gray-200 pt-6'>
-            <dt className='text-base font-medium'>Toplam</dt>
-            <dd className='text-base font-medium text-gray-900'>$69.00</dd>
-          </div>
-        </dl>
-
-        <div className='border-t border-gray-200 px-4 py-6 sm:px-6'>
+        {item.length > 0 && (
+          <dl className="space-y-6 border-t border-gray-200 px-4 py-6 sm:px-6">
+            <div className="flex items-center justify-between">
+              <dt className="text-sm">Ara Toplam</dt>
+              <dd className="text-sm font-medium text-gray-900">
+                $
+                {item
+                  .reduce((sum, item, index) => {
+                    sum += item.amount * item.birim
+                    return sum
+                  }, 0)
+                  .toFixed(2)}
+              </dd>
+            </div>
+            <div className="flex items-center justify-between">
+              <dt className="text-sm">Kargo</dt>
+              <dd className="text-sm font-medium text-gray-900">
+                $
+                {item
+                  .reduce((sum, item, index) => {
+                    if (item.amount > 0) {
+                      sum += Number(item.cargo.split('$')[1])
+                    }
+                    return sum
+                  }, 0)
+                  .toFixed(2)}
+              </dd>
+            </div>
+            <div className="flex items-center justify-between border-t border-gray-200 pt-6">
+              <dt className="font-extrabold text-xl text-amber-800">Toplam</dt>
+              <dd className="text-base font-medium text-gray-900">
+                $
+                {item
+                  .reduce((sum, item, index) => {
+                    if (item.amount > 0) {
+                      return (
+                        sum +
+                        Number(item.cargo.split('$')[1]) +
+                        Number(item.amount * item.birim)
+                      )
+                    }
+                    return sum
+                  }, 0)
+                  .toFixed(2)}
+              </dd>
+            </div>
+          </dl>
+        )}
+        {item.length == 0 && <p className="text-center">Sepetiniz boş</p>}
+        <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
           <button
-            type='submit'
-            className='w-full rounded-md border border-transparent bg-indigo-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50'
+            type="submit"
+            className="w-full rounded-md border border-transparent bg-indigo-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+            onClick={handleClick}
           >
             Siparişi onaylayın
           </button>
